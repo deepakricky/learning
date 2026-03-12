eac1be25b1b3d0a9c5eb66ed0f244b42ce8a2823
Deepak Kumar Teegala <57917822+deepakricky@users.noreply.github.com>
2026-03-12 18:53:06 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,61 @@
+##for loop
+# for i in range(1, 6):
+#     print(i)
+
+##while loop
+# print("\n")
+# j=1
+# while(j<=10):
+#     print(j)
+#     j+=1 #same as j=j+1
+
+## Write a program to print the content of a list using while loops.
+
+# list=["Deepak", 1, 22.999,"noob","kites", "Roger"]
+# i=0
+# while(i<len(list)):
+#     print(list[i])
+#     i+=1
+
+##range functions
+# for i in range(0,100,5): ## range(100) can also be used. here 0 is start 100 is end and 5 is skip
+#     print(i)
+
+##for loop iterate
+# list=[1,23,4,5]
+# for i in list:
+#     print(i)
+
+# tuple=(1,4,5,5,5,6)
+# for j in tuple:
+#     print(j)
+
+# str="deepak"
+# for i in str:
+#     print(i)
+
+## for loop with else
+# l=["hello", 1, 2233, 66]
+# for i in l:
+#     print(i)
+# else:
+#     print("The list is completed") #this is printed when the loop is exhausts
+
+##break and continue
+# for i in range(100):
+#     if i==35:
+#         break #exit the loop right now
+#     print(i)
+
+# for i in range(100):
+#     if i==35:
+#         continue #skip this iteration, removes 35
+#     print(i)
+
+##pass
+for i in range(5):
+    pass #skips the for function for now
+i=0
+while(i<10):
+    print(i)
+    i+=1

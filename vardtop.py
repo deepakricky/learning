f6dc6ebca21e8b80e28cc653535a736c6bf35afb
Deepak Kumar Teegala <57917822+deepakricky@users.noreply.github.com>
2026-03-12 09:45:14 +0530

    Create vardtop.py
    revised variables, datatypes and operators

---
+++ vardtop.py
@@ -0,0 +1,59 @@
+#variables
+a = 1
+b = 2
+c = 7
+name = "deepak"
+print(a + b)
+#datatypes
+a = 1 # a is an integer
+b = 5.22 # b is a floating point number
+c = "Gagga" # c is a string
+d = False # d is a boolean variable
+e = None # e is a none type variable
+#datatype rules
+a = 23
+aaa = 435
+hello345 = 34
+deepak = 45
+_deepak = 34#above all are valid
+@deepak = 56 # Invalid due to @ symbol
+deep@k # Invalid due to @ symbol
+#Operators
+# Arithmetic Operators
+a = 7
+b = 4
+c = a + b
+print(c)
+
+# Assignment Operators
+a = 4-2 # Assign 4-2 in a
+print(a)
+b = 6
+# b += 3 # Increment the value of b by 3 and then assign it to b
+b -= 3 # Decrement the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison Operators
+
+d = 5==5
+
+print(d)
+
+
+# Logical Operators
+
+e = True or False
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and' 
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(True))

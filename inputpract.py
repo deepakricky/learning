8f171c2427581a95fa049983dc9131b9ece1a74f
Deepak Kumar Teegala <57917822+deepakricky@users.noreply.github.com>
2026-03-12 10:52:07 +0530

    Create inputpract.py
    practiced input function

---
+++ inputpract.py
@@ -0,0 +1,18 @@
+# a= input("enter name:")
+# print(a)
+# b=int(input("enter number"))
+# print("the number is: ",b)
+
+# a=int(input("Enter 1st number: "))
+# b=int(input("Enter 2nd number: "))
+# print("The sum of both the numbers is: ",a+b)
+# print("The remainder when a is divided by b is: ",a%b)
+
+# c=input("Enter the value of c: ")
+# print(type(c)) 
+
+d=int(input("1st number: "))
+e=int(input("2nd num: "))
+print("d is greater than e ? :",d>e)
+print("the average these 2 numbers is: ",(d+e)/2)
+print("square of d is :", d**2)

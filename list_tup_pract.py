b2ebdee1245a3b9270d59741244237f9b8adb7b2
Deepak Kumar Teegala <57917822+deepakricky@users.noreply.github.com>
2026-03-12 13:19:34 +0530

    Create list_tup_pract.py
    practiced few questions from online

---
+++ list_tup_pract.py
@@ -0,0 +1,28 @@
+
+# # take input of marks and sort them, this is cause of starting learning of lists
+# #later we will use for i in range(6)....
+
+# marks=[]
+# f1=int(input("Enter marks: "))
+# marks.append(f1)
+# f3=int(input("Enter marks: "))
+# marks.append(f3)
+# f3=int(input("Enter marks: "))
+# marks.append(f3)
+# f4=int(input("Enter marks: "))
+# marks.append(f4)
+# f5=int(input("Enter marks: "))
+# marks.append(f5)
+# f6=int(input("Enter marks: "))
+# marks.append(f6)
+# marks.sort()
+# print(marks)
+
+#sum of numbers in list
+list=[1,2,3,4]
+print(sum(list))
+
+#check num of zeroes
+a = (7, 0, 8, 0, 0, 9)
+b=a.count(0)
+print(b)

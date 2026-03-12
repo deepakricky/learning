aad0e53c1bc1de41afce48a02f837c588c5fd37b
Deepak Kumar Teegala <57917822+deepakricky@users.noreply.github.com>
2026-03-12 12:57:15 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,10 @@
+#tuple are immutable dataype
+b=()#empty tuple
+a=('hello',1,25,5)
+c=(1,) #tuple with only one element
+print(type(c))
+#methods
+print(a)
+print(a.count(1)) # how many time 1 in tuple a
+i = a.index(25)
+print(i)

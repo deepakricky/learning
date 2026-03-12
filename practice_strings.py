578ea738206328c0cccdf2e4b5ce32f43145ab31
Deepak Kumar Teegala <57917822+deepakricky@users.noreply.github.com>
2026-03-12 12:01:04 +0530

    Create practice_strings.py

---
+++ practice_strings.py
@@ -0,0 +1,16 @@
+# name=input("Enter your name: ")
+# print("Good afternoon ",name)
+# #new way
+# print(f"Good afternoon {name}")
+
+# letter = ''' Dear <|Name|>, 
+# You are selected!
+# <|Date|> '''
+# print(letter.replace("<|Name|>","Deepak").replace("<|Date|>"," 12th March"))
+
+# hello= "hi  how  are you"
+# print(hello.find("  "))
+# print(hello.replace("  "," "))
+
+note= "Dear gamers, \nThe game will be released tomorrow. \nThanks!"
+print(note)

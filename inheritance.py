4fa8e6e90734c6f04385b2b30b561bc6c77f011a
Deepak Kumar Teegala <57917822+deepakricky@users.noreply.github.com>
2026-03-14 12:30:31 +0530

    Update inheritance.py
    learnt inheritance, and how to acquire info from parent class to child class

--- inheritance.py
+++ inheritance.py
@@ -1,14 +1,15 @@
 class Employee: #Base class(Parent class)
     company= "ITC"
-    
+    name="Deepak"
     def show(self):
         print(f"The name of the employee is {self.name} and salary is {self.salary}")
 
 class Programmer(Employee): #child class
-    company="ITC infotech"
+    company= "ITC infotech"
+    language="Python"
     def showLanguage(self):
         print(f"The name is {self.name} and he knows {self.language} language")
 
 a=Programmer()
-
 print(a.company)
+print(a.name) # here we can see im printing name from child class which was first in parent class
